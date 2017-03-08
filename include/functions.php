227ad4d797b1ae9b4e338910600bdeaee53fbe14
Kévin Guiot <contact@kevinguiot.fr>
2017-03-08 17:15:23 +0100

    Rajout d'une fonction pour pouvoir récupérer les information d'un utilisateur.

--- include/functions.php
+++ include/functions.php
@@ -9,4 +9,30 @@ function connect() {
     }
 }
 
+//Fonction pour récupérer une information d'un utilisateur
+function getUser($avant, $info, $apres) {
+    global $connexion;
+    
+    //Déclarations
+    $valeurDemandee = false;
+    
+    //On fait la requête pour récupérer l'information de l'utilisateur
+    $req_selectUser = $connexion->prepare('SELECT * FROM membres WHERE '.$avant.' = :info');
+    $req_selectUser->execute(array(
+        'info' => $info,
+    ));
+    $nbr_selectUser = $req_selectUser->rowCount();
+    
+    //Si l'utilisateur existe
+    if($nbr_selectUser == 1) {
+        //On récupère les informations
+        $selectUser = $req_selectUser->fetch();
+        
+        //On récupère la valeur de la donnée demandée
+        $valeurDemandee = $selectUser[$apres];
+    }
+    
+    return $valeurDemandee;
+}
+
 ?>
