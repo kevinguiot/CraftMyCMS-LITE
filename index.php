e2ffed239fee4e145f3a9c4fad2603f57fca0e36
Kévin Guiot <contact@kevinguiot.fr>
2017-03-08 17:24:29 +0100

    Ajout des news sur la page d'accueil.

--- index.php
+++ index.php
@@ -1,9 +1,33 @@
 <?php
 include('include/init.php');
 include('include/header.php');
-
-echo "contenu";
-
-include('include/menudroite.php');
-include('include/footer.php');
 ?>
+<div id="contenu">
+    <div id="news">
+        <?php
+        $req_selectNews = $connexion->query('SELECT * FROM news ORDER BY id DESC');
+        $nbr_selectNews = $req_selectNews->rowCount();
+        
+        if($nbr_selectNews > 0) {
+            while($selectNews = $req_selectNews->fetch()) {
+                
+                $content = str_replace("\n", '<br>', $selectNews['content']);
+                
+                echo '<div class="news_content">';
+                
+                if(!empty($selectNews['titre'])) {
+                    echo '<strong>'.$selectNews['titre'].'</strong><br>';
+                }
+                
+                
+                echo $content.'<br>';
+                echo '<br>Publiée par <strong>'.$selectNews['pseudo'].'</strong>, le '.$selectNews['date'].' à '.$selectNews['heure'].'.';
+                echo '</div>';
+            }
+        }
+        ?>
+    </div>
+    <?php
+    include('include/menudroite.php');
+    include('include/footer.php');
+    ?>
